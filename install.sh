d7c64319ca1b1e283d4c923b92aa5f8fb40baf5a
Jeff French <jeff.french@moonswitch.com>
2022-02-16 16:00:19 -0600

    Rename installer.sh to install.sh for Coder compatability

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-
-
 # git
 #ln -s ${BASEDIR}/.git ~/
 ln -s ${BASEDIR}/.gitconfig ~/
@@ -20,21 +18,21 @@ ln -s ${BASEDIR}/.wgetrc ~/
 
 #bashy
 ln -s ${BASEDIR}/.aliases ~/
-ln -s ${BASEDIR}/.bash_profile ~/
-ln -s ${BASEDIR}/.bashrc ~/
-ln -s ${BASEDIR}/.bash_colors ~/
-ln -s ${BASEDIR}/.git-completion.bash ~/
-ln -s ${BASEDIR}/.git-prompt.sh ~/
+#ln -s ${BASEDIR}/.bash_profile ~/
+#ln -s ${BASEDIR}/.bashrc ~/
+#ln -s ${BASEDIR}/.bash_colors ~/
+#ln -s ${BASEDIR}/.git-completion.bash ~/
+#ln -s ${BASEDIR}/.git-prompt.sh ~/
+
+# zsh
+# TODO: add zsh and omyzsh configs
 
 #vim
-ln -s ${BASEDIR}/.vim ~/
-ln -s ${BASEDIR}/.vimrc ~/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+#ln -s ${BASEDIR}/.vim ~/
+#ln -s ${BASEDIR}/.vimrc ~/
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall +qall
 
 #postgres
 ln -s ${BASEDIR}/.psqlrc ~/
-
-#hostfile
-ln -s ${BASEDIR}/host_file_update.sh ~/
 
